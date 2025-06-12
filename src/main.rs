8f0184357b5a2e8c85f5ac379c19ce842c9ba29a
The-Orange-Dot <theorangedottwitch@gmail.com>
2025-06-12 08:54:01 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -93,9 +93,10 @@ async fn main() -> std::io::Result<()> {
                     .allowed_origin("https://localhost:3000") 
                     .allowed_origin("http://localhost:3000") 
                     .allowed_origin("http://100.74.191.99:3000") 
-                    .allowed_origin("100.74.191.99") 
+                    .allowed_origin("https://100.74.191.99:3000") 
                     .allowed_methods(["POST", "DELETE", "GET", "PUT", "OPTIONS"])
-                    .allowed_headers(vec!["Content-Type", "Authorization", "Accept"]) // Headers your frontend sends
+                    //.allowed_headers(vec!["Content-Type", "Authorization", "Accept"]) // Headers your frontend sends
+                    .allow_any_header()
                     .supports_credentials() 
                     .max_age(3600)
             )
